3b830a748895574bbb6532b78e2bd4e36aca6ae4
btsui <btsui4@gmail.com>
2020-12-25 22:55:43 -0700

    Refactor Day 4

--- 2020/Golang/days/day4.go
+++ 2020/Golang/days/day4.go
@@ -2,163 +2,134 @@ package days
 
 import (
 	"fmt"
-	"strconv"
+	"regexp"
 	"strings"
-	"unicode"
 )
 
-// Four : advent of code, day four part1 and 2
+//Four is AOC Day 4
 func Four() {
+	println(fmt.Sprintf("Part 1: %d", part1(GetInput(4))))
+	println(fmt.Sprintf("Part 2: %d", part2(GetInput(4))))
+}
+func part1(input string) int {
+	passportObject := convertToObject(input)
+
 	requiredFields := []string{
 		"byr",
 		"iyr",
 		"eyr",
 		"hgt",
+		"hcl",
 		"ecl",
 		"pid",
-		"hcl",
-		"cid",
+		// "cid", // Part 1
 	}
-
-	input := GetInput(4)
-
-	passports := strings.Split(input, "\n\n")
-	valid := 0
-	completeMatch := false
-
-	for i := 0; i < len(passports); i++ {
-		for field := 0; field < len(requiredFields); field++ {
-			if requiredFields[field] == "cid" {
-				continue
-			}
-
-			if !strings.Contains(passports[i], requiredFields[field]) {
-				completeMatch = false
-				break
-			} else if !isFieldValid(requiredFields[field], passports[i]) {
-				completeMatch = false
+	var ans int
+	for _, passportEntry := range passportObject {
+		hasAllFields := true
+		for _, field := range requiredFields {
+			// Default Zero Value for String
+			if passportEntry[field] == "" {
+				hasAllFields = false
 				break
 			}
-			completeMatch = true
 		}
-		if completeMatch {
-			valid++
-			completeMatch = false
+		if hasAllFields {
+			ans++
 		}
 	}
-	fmt.Print("Number of valid passports reported: ")
-	fmt.Println(valid)
+
+	return ans
 }
 
-func isFieldValid(field string, passport string) bool {
-	passport = strings.Replace(passport, "\n", " ", -1)
-	fields := strings.Split(passport, " ")
-	for fieldIn := 0; fieldIn < len(fields); fieldIn++ {
-		fields[fieldIn] = strings.Replace(fields[fieldIn], "\t", "", -1)
-		if strings.Contains(fields[fieldIn], field) {
-			fields[fieldIn] = strings.Split(fields[fieldIn], ":")[1]
-			switch field {
-			case "byr":
-				return byr(fields[fieldIn])
-			case "iyr":
-				return iyr(fields[fieldIn])
-			case "eyr":
-				return eyr(fields[fieldIn])
-			case "hgt":
-				return hgt(fields[fieldIn])
-			case "ecl":
-				return ecl(fields[fieldIn])
-			case "pid":
-				return pid(fields[fieldIn])
-			case "hcl":
-				return hcl(fields[fieldIn])
-			case "cid":
-				return true
-			default:
-				fmt.Println("not a field")
-			}
+func part2(input string) int {
+	passports := convertToObject(input)
+
+	var ans int
+	for _, entry := range passports {
+		if validateFields(entry) {
+			ans++
 		}
 	}
-	return false
+
+	return ans
 }
 
-func hgt(in string) bool {
-	// a number followed by either cm or in:
-	// If cm, the number must be at least 150 and at most 193.
-	// If in, the number must be at least 59 and at most 76.
+func convertToObject(input string) []map[string]string {
+	var passports []map[string]string
 
-	if strings.Contains(in, "cm") {
-		in = strings.ReplaceAll(in, "cm", "")
-		asInt, _ := strconv.Atoi(in)
-		return asInt >= 150 && asInt <= 193
-	} else if strings.Contains(in, "in") {
-		in = strings.ReplaceAll(in, "in", "")
-		asInt, _ := strconv.Atoi(in)
-		return asInt >= 59 && asInt <= 76
-	} else {
+	// Separate by empty line
+	lines := strings.Split(input, "\n\n")
+
+	for _, line := range lines {
+		passportDetails := map[string]string{}
+
+		oneLine := strings.ReplaceAll(line, "\n", " ")
+		for _, entry := range strings.Split(oneLine, " ") {
+			splitEntry := strings.Split(entry, ":")
+			passportDetails[splitEntry[0]] = splitEntry[1]
+		}
+		passports = append(passports, passportDetails)
+	}
+
+	return passports
+}
+
+func validateFields(entry map[string]string) bool {
+	byr := entry["byr"]
+	if byr > "2002" || byr < "1920" || len(byr) != 4 {
 		return false
 	}
-}
 
-func byr(in string) bool {
-	// four digits; at least 1920 and at most 2002.
-	asInt, _ := strconv.Atoi(in)
-	return asInt >= 1920 && asInt <= 2002
-}
+	iyr := entry["iyr"]
+	if iyr > "2020" || iyr < "2010" || len(iyr) != 4 {
+		return false
+	}
 
-func iyr(in string) bool {
-	// four digits; at least 2010 and at most 2020.
-	asInt, _ := strconv.Atoi(in)
-	return asInt >= 2010 && asInt <= 2020
-}
+	eyr := entry["eyr"]
+	if eyr > "2030" || eyr < "2020" || len(eyr) != 4 {
+		return false
+	}
 
-func eyr(in string) bool {
-	// four digits; at least 2020 and at most 2030.
-	asInt, _ := strconv.Atoi(in)
-	return asInt >= 2020 && asInt <= 2030
-}
-func hcl(in string) bool {
-	// a # followed by exactly six characters 0-9 or a-f.
-	allGood := false
-	if strings.HasPrefix(in, "#") {
-		in = strings.ReplaceAll(in, "#", "")
-		if !(len(in) == 6) {
+	hgt := entry["hgt"]
+	if hgt == "" {
+		return false
+	}
+
+	var num int
+	var unit string
+	fmt.Sscanf(hgt, "%d%s", &num, &unit)
+
+	switch unit {
+	case "cm":
+		if num < 150 || num > 193 {
 			return false
 		}
-		toRune := []rune(in)
-
-		for r := 0; r < len(toRune); r++ {
-			allGood = (unicode.IsDigit(toRune[r]) || unicode.IsLetter(toRune[r]))
+	case "in":
+		if num < 59 || num > 76 {
+			return false
 		}
-	}
-	return allGood
-}
-
-func ecl(in string) bool {
-	// exactly one of: amb blu brn gry grn hzl oth.
-	correctVals := []string{
-		"amb",
-		"blu",
-		"brn",
-		"gry",
-		"grn",
-		"hzl",
-		"oth",
-	}
-
-	for _, val := range correctVals {
-		if val == in {
-			return true
-		}
-	}
-	return false
-}
-
-func pid(in string) bool {
-	// a nine-digit number, including leading zeroes.
-	if !(len(in) == 9) {
+	default:
 		return false
 	}
-	_, err := strconv.Atoi(in)
-	return err == nil
+
+	hcl := entry["hcl"]
+	reg := regexp.MustCompile("^#[0-9a-f]{6}$")
+	if !reg.Match([]byte(hcl)) {
+		return false
+	}
+
+	ecl := entry["ecl"]
+	reg = regexp.MustCompile("^(amb|blu|brn|gry|grn|hzl|oth)$")
+	if !reg.Match([]byte(ecl)) {
+		return false
+	}
+
+	pid := entry["pid"]
+	reg = regexp.MustCompile("^[0-9]{9}$")
+	if !reg.Match([]byte(pid)) {
+		return false
+	}
+
+	return true
 }
